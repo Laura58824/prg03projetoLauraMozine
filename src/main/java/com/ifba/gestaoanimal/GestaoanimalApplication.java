128ae635cd57fe876def9cc2a1c523071b37d7eb
Laura58824 <lauramozine@gmail.com>
2026-05-31 22:23:14 -0300

    feat: adiciona enumerações do sistema de gestão animal

---
+++ src/main/java/com/ifba/gestaoanimal/GestaoanimalApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.gestaoanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoanimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoanimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifba/gestaoanimal/GestaoanimalApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ifba.gestaoanimal;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Desabilitado até configurar o banco")
+class GestaoanimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
