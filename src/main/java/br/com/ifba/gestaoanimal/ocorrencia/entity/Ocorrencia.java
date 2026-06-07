4ba237b1a1c8095705a67cf19d43523514c0053a
Laura58824 <lauramozine@gmail.com>
2026-06-07 19:35:42 -0300

    feat: add entity Ocorrencia

--- src/main/java/br/com/ifba/gestaoanimal/ocorrencia/entity/Ocorrencia.java
+++ src/main/java/br/com/ifba/gestaoanimal/ocorrencia/entity/Ocorrencia.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.com.ifba.gestaoanimal.ocorrencia.entity;
 
-/**
- *
- * @author m
- */
-public class Ocorrencia {
-    
+import br.com.ifba.gestaoanimal.infrastructure.entity.PersistenceEntity;
+import br.com.ifba.gestaoanimal.pessoa.entity.Pessoa;
+import br.com.ifba.gestaoanimal.enums.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "ocorrencia")
+
+public class Ocorrencia extends PersistenceEntity {
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private TipoOcorrenciaEnum tipo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "urgencia", nullable = false)
+    private UrgenciaEnum urgencia;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StatusOcorrenciaEnum status;
+
+    @Column(name = "data_registro", nullable = false)
+    private LocalDateTime dataRegistro;
+
+    @Column(name = "data_atendimento")
+    private LocalDateTime dataAtendimento;
+
+    @ManyToOne
+    @JoinColumn(name = "registrada_por_id", nullable = false)
+    private Pessoa registradaPor;
+
+    @ManyToOne
+    @JoinColumn(name = "voluntario_id")
+    private Pessoa voluntario;
+
 }
