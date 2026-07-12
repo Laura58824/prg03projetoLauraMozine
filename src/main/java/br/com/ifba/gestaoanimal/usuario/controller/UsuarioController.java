ba038ca9a844cd1be675c051b92e32b53f9caaca
Laura58824 <lauramozine@gmail.com>
2026-07-12 20:34:14 -0300

    feat: modifica o main

--- src/main/java/br/com/ifba/gestaoanimal/usuario/controller/UsuarioController.java
+++ src/main/java/br/com/ifba/gestaoanimal/usuario/controller/UsuarioController.java
@@ -7,35 +7,42 @@ import org.springframework.stereotype.Controller;
 import java.util.List;
 
 @Controller
-public class UsuarioController {
+public class UsuarioController implements UsuarioIController{
 
     @Autowired
     private UsuarioIService usuarioService;
 
+    @Override
     public Usuario save(Usuario usuario) {
         return usuarioService.save(usuario);
     }
 
+    @Override
     public Usuario update(Usuario usuario) {
         return usuarioService.update(usuario);
     }
 
+    @Override
     public void delete(Long id) {
         usuarioService.delete(id);
     }
 
+    @Override
     public List<Usuario> findAll() {
         return usuarioService.findAll();
     }
 
+    @Override
     public List<Usuario> findByAtivoTrue() {
         return usuarioService.findByAtivoTrue();
     }
 
+    @Override
     public Usuario findById(Long id) {
         return usuarioService.findById(id);
     }
 
+    @Override
     public Usuario autenticar(String login, String senha) {
         return usuarioService.autenticar(login, senha);
     }
